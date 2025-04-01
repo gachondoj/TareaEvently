582835cfb7361fe5986f8fbd0cbaff5d36806bb3
gachondoj <gachondj@gmail.com>
2025-04-01 13:27:14 -0300

    event button and style changes

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+
+export default function Admin() {
+    return (
+      <div className="justify-items-center flex flex-col justify-between min-h-screen font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[32px] p-5 items-center sm:items-start w-full">
+          <div className="text-[#a855f7] font-bold">ADMIN</div>
+
+          <div className="flex gap-5 w-full justify-center">
+            <Button asChild className="hover:bg-[#9378ff] bg-[#a855f7]">
+              <a href="/admin/events">Agregar Evento</a>
+            </Button>
+            <Button asChild className="hover:bg-[#9378ff] bg-[#a855f7]">
+              <a href="/admin/tickets">Ver Tickets</a>
+            </Button>
+          </div>
+        </main>
+        <footer className="w-full flex justify-end pb-5 px-5">
+          <Button asChild>
+            <a href="/">
+              Volver
+            </a>
+          </Button>
+        </footer>
+      </div>
+    )
+}
---
+++ src/app/events/page.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+
+export default async function Events() {
+
+  const events = await prisma.event.findMany();
+
+    return (
+      <div className="justify-items-center flex flex-col justify-between min-h-screen font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[32px] p-5 items-center sm:items-start w-full">
+            <div className="text-[#a855f7] font-bold">EVENTOS</div>
+
+            <div className="grid grid-cols-3 gap-y-5 gap-x-5">
+              {events.map((event) => (
+                <Button key={event.id} asChild className="h-[5rem] w-[25rem] hover:bg-[#9378ff] bg-[#a855f7]">
+                    <a href={`/events/${event.id}`} className="flex flex-col items-start">
+                      <h2>{event.title}</h2>
+                      <div className="flex justify-between w-full">
+                        <p>📅 {new Date(event.date).toLocaleDateString()}</p>
+                        <p>💰 Precio: ${event.price}</p>
+                      </div>
+                  </a>
+                </Button>
+              ))}
+            </div>
+        </main>
+        <footer className="w-full flex justify-end pb-5 px-5">
+          <Button asChild>
+            <a href="/">
+              Volver
+            </a>
+          </Button>
+        </footer>
+      </div>
+    )
+}
