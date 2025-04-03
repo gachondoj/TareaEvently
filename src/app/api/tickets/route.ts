a555d0284b37d619683371a71216ab70541cde18
gachondoj <gachondj@gmail.com>
2025-04-03 15:36:02 -0300

    restore post method on tickets

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -33,3 +33,39 @@ export async function GET(req: Request) {
     tickets,
   });
 }
+
+export async function POST(req: Request) {
+  try {
+    const { tickets } = await req.json();
+    if (!Array.isArray(tickets) || tickets.length === 0) {
+      return NextResponse.json({ error: "Se requiere una lista de tickets" }, { status: 400 });
+    }
+
+    const createdTickets = await prisma.$transaction(async (tx) => {
+      return Promise.all(
+        tickets.map(async ({ eventId, buyerId }) => {
+          const event = await tx.event.findUnique({ where: { id: eventId } });
+
+          if (!event) throw new Error(`Evento con ID ${eventId} no encontrado`);
+          if (event.availableRoom <= 0) throw new Error(`Evento ${event.title} agotado`);
+
+          const ticket = await tx.ticket.create({
+            data: { eventId, buyerId },
+          });
+
+          await tx.event.update({
+            where: { id: eventId },
+            data: { availableRoom: event.availableRoom - 1 },
+          });
+
+          return ticket;
+        })
+      );
+    });
+
+    return NextResponse.json(createdTickets, { status: 201 });
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error:  "Error en el servidor" }, { status: 500 });
+  }
+}
